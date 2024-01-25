d38a234e2a04fb23b30fa9a93f0d7cc856ea733e
Rafael Inoue <rafael.niyasue@senacsp.edu.br>
2024-01-25 02:09:36 -0300

    agora só estilizar

---
+++ backup.php
@@ -0,0 +1,56 @@
+<!-- Para 2 tópicos -->
+<table>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault1"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="a"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault2"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="b"></label> </td>
+    </tr>
+</table>
+<!-- Para 3 tópicos -->
+<table>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault1"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="a"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault2"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="b"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault3"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" value="c"></label> </td>
+    </tr>
+</table>
+<!-- Para 4 tópicos -->
+
+<table>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault1"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="a"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault2"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="b"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault3"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" value="c"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault4"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4" value="d"></label> </td>
+    </tr>
+</table>
+
+<!-- Para tópicos com resposta -->
+
+<table>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault1"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" value="a"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault2"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" value="b"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault3"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" value="c"></label> </td>
+    </tr>
+    <tr>
+        <td><label class="form-check-label" for="flexRadioDefault4"><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4" value="d"></label>
+            <input type="text" name="outros_especifique" placeholder="Especifique">
+        </td>  
+    </tr>
+</table>
---
+++ includes/cabecalho.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <div id="barra-topo">
+        <header id="cabecalho" class="container">
+            <div id="logo" class="container">
+                <a href=""><img src="" alt="Logo"></a>
+            </div>
+
+            <nav id="informacoes">
+                <ul>
+                    <li><a href="#">Tabela de Preços e Serviços</a></li>
+                    <li>Contato em (14)99882-3653</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
